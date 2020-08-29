cf356719c995a3d95203c85e6822b440763c88d8
Daniel Yagodaev <danielyag@gmail.com>
2020-08-29 15:07:32 +0300

    Updates

---
+++ precache-manifest.aec730b524f7b76ae5ff2e61dfefffd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53ccb144e77107dc8253daec2d807c8",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "bc355f9f7b68189858c5",
+    "url": "/profile/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "4efe8539c8844a110c39",
+    "url": "/profile/static/js/2.3f1c621a.chunk.js"
+  },
+  {
+    "revision": "c3e488c1b6c2872635b113f5babd035a",
+    "url": "/profile/static/js/2.3f1c621a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc355f9f7b68189858c5",
+    "url": "/profile/static/js/main.c4edeb1f.chunk.js"
+  },
+  {
+    "revision": "5076af70b647b2393d8e",
+    "url": "/profile/static/js/runtime-main.8771bb24.js"
+  }
+]);
