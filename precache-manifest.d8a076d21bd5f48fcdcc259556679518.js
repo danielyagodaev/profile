564d8da636f8f36307b20facb96f06f56d3eb94b
Daniel Yagodaev <danielyag@gmail.com>
2020-10-03 12:23:17 +0300

    Updates

--- precache-manifest.d8a076d21bd5f48fcdcc259556679518.js
+++ precache-manifest.d8a076d21bd5f48fcdcc259556679518.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "abfb2b7d20dd09ab1811d002a07950a9",
+    "revision": "37aee7bda1631f9fa796c9b53db141dc",
     "url": "/profile/index.html"
   },
   {
-    "revision": "0162fe53d4662b55249f",
+    "revision": "e029b8c679a4be423322",
     "url": "/profile/static/css/main.b1110ec6.chunk.css"
   },
   {
-    "revision": "ffefc2bcfe026a469090",
-    "url": "/profile/static/js/2.c9f16123.chunk.js"
+    "revision": "0feb5450de4141fe8971",
+    "url": "/profile/static/js/2.c279f1c8.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/profile/static/js/2.c9f16123.chunk.js.LICENSE.txt"
+    "url": "/profile/static/js/2.c279f1c8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0162fe53d4662b55249f",
-    "url": "/profile/static/js/main.9754672a.chunk.js"
+    "revision": "e029b8c679a4be423322",
+    "url": "/profile/static/js/main.83acaad2.chunk.js"
   },
   {
     "revision": "5076af70b647b2393d8e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.3e5a843634ea0e3a3df3655c053c6c42.js"
+  "/profile/precache-manifest.d8a076d21bd5f48fcdcc259556679518.js"
 );
 
 self.addEventListener('message', (event) => {
